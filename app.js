f1baf50728bc1d677c0e3df28acef6cbc6a84918
Yohan Totting <tyohan@gmail.com>
2015-08-28 09:58:28 +0630

    add recursive function to convert all image in a directory

--- app.js
+++ app.js
@@ -1,58 +1,77 @@
 var cv = require('./node_modules/opencv/lib/opencv');
-
+var fs = require('fs');
 var lowThresh =10;
 var highThresh = 150;
 var nIters = 2;
 var maxArea = 2500;
 
+var path='./color_images';
 var GREEN = [0, 255, 0]; // B, G, R
 var WHITE = [255, 255, 255]; // B, G, R
 var RED   = [0, 0, 255]; // B, G, R
+var isImage=function(image){
+    image=image.toLowerCase();
+    return image.indexOf('jpg')!==-1 || image.indexOf('png')!==-1;
+}
 
-cv.readImage('./form.jpg', function(err, im) {
-  if (err) throw err;
-  var width = im.width();
-  var height = im.height();
-  if (width < 1 || height < 1) throw new Error('Image has no size');
+String.prototype.getFileName = function() {
+    var pos = this.lastIndexOf('/');
+    return this.substring(pos+1);
+}
 
-  var big = new cv.Matrix(height/2, width/2);
-
-  im=im.roi(width/2, 0, width/2, height/2)
-  im_canny = im.copy();
-  im_canny.convertGrayscale();
-  //im.gaussianBlur([5,5]);
-  im_canny.canny(lowThresh, highThresh);
-  im_canny.dilate(nIters);
-
-  contours = im_canny.findContours();
-
-  var max_i=null;
-  var ratio=0
-  var rect=null;
-  for(i = 0; i < contours.size(); i++) {
-  	
-    if(contours.area(i) > maxArea) {
-      max_i=i;
-      maxArea=contours.area(i);
-      rect=contours.boundingRect(i);
-      ratio=rect.width/rect.height;
-      console.log(rect);
-      console.log(ratio);
-      if(ratio>0.5 && ratio<2){
-      	  console.log('Saved countour: '+i);
-		  var moments = contours.moments(i);
-		  var cgx = Math.round(moments.m10 / moments.m00);
-		  var cgy = Math.round(moments.m01 / moments.m00);
-		  big.drawContour(contours, i, GREEN);
-		  big.line([cgx - 5, cgy], [cgx + 5, cgy], RED);
-		  big.line([cgx, cgy - 5], [cgx, cgy + 5], RED);
-		  im.rectangle([rect.x, rect.y], [rect.width, rect.height], GREEN, 2);
-      }
-      
-    }
-  }
-  
-  im.save('./detect.png');
-  big.save('./big.png');
-  console.log('Image saved to ./big.png');
+fs.readdir(path, function(err, files){
+        cropPhoto(files)
+          
+   
 });
+
+var cropPhoto=function(files){
+    var image=files.pop();
+
+          console.log('Checking image '+image);
+          console.log(isImage(image));
+          if(isImage(image)){
+              var file=path+'/'+image;
+               console.log('detecting face on file '+image);
+               cv.readImage(file, function(err, im) {
+                if (err) throw err;
+                var width = im.width();
+                var height = im.height();
+                if (width < 1 || height < 1) throw new Error('Image has no size');
+
+                im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+                  if (err) throw err;
+                  image=image.replace('jpg','png');
+                  var facepath='./tmp/'+image;
+                 
+                  console.log('Found '+faces.length+' face\'s on '+image );
+                  var biggest=0;
+                  var bigest_face=null;
+                  
+                  for (var i = 0; i < faces.length; i++) {
+                    var area=faces[i].width*faces[i].height;
+                    if(area>biggest){
+                      biggest=area;
+                      bigest_face=faces[i];
+                    }
+                  }
+
+                  if(faces.length>0 && bigest_face!==null){
+                    var padding=Math.round(bigest_face.width/3);
+                    var imWidth=bigest_face.width+(padding*2);
+                    var imHeight=Math.round(imWidth*(4/3));
+                    im=im.roi(bigest_face.x-padding, bigest_face.y-padding, imWidth, imHeight);
+                    //console.log('Image saved to '.face);
+                    console.log(facepath);
+                    im.save(facepath);
+                  }
+                  
+                  console.log('Files left:'+files.length);
+                  if(files.length>0)
+                      cropPhoto(files);
+                  
+                });
+
+              });
+          }
+}
